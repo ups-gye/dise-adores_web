33abc2919685f9f71238c6d044812e4a0aeb08bd
Sulay Valeria Vergara Chávez <svergarac@est.ups.edu.ec>
2024-08-24 11:48:04 -0500

    limpieza

--- PROTOTIPO/js/juego.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('startButton').addEventListener('click', function() {
-    // Oculta la imagen principal
-    document.getElementById('imageContainer').classList.add('hidden');
-
-    // Muestra la nueva imagen
-    document.getElementById('newImageContainer').classList.remove('hidden');
-});
