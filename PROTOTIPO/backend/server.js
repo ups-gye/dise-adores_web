396b9e9bd8e1eae00eba4a81a7cb1c3da7aea197
Sulay Valeria Vergara Chávez <svergarac@est.ups.edu.ec>
2024-08-24 11:46:49 -0500

    direccion-servidor

--- PROTOTIPO/backend/server.js
+++ PROTOTIPO/backend/server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname,'..', 'frontend')));
 
 // Ruta para el index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname,'..', 'frontend', 'datosprincipales.html'));
 });
 
 routes( app )
